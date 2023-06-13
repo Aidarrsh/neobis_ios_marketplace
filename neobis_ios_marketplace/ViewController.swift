d24c8bf36a04ffa9783f3c075e768a9d9ad37f86
aidar <sharipov_ai@auca.kg>
2023-06-13 20:05:10 +0600

    Initial Commit

---
+++ neobis_ios_marketplace/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  neobis_ios_marketplace
+//
+//  Created by Айдар Шарипов on 13/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
